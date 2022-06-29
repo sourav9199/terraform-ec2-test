2700ee9493d9fa2e089bfc7fff6a06c91023ea19
sourav9199 <108008862+sourav9199@users.noreply.github.com>
2022-06-29 11:28:23 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 variable "aws_region"{
-    default= "ap-south-1"
+    type = string
 }
 variable "ec2_ami"{
-    default = "ami-08df646e18b182346"
+    type = string
 }
 variable "instance_type" {
-    default = "t2.micro"
+    type = string
 }
